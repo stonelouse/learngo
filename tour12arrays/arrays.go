a814783a4a01664a146f09e5443887929a2f6023
Christian Russ <russ.c@eplan.de>
2019-09-22 15:42:14 +0200

    Arrays

---
+++ tour12arrays/arrays.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// The type [n]T is an array of n values of type T.
+	// An array's length is part of its type, so arrays cannot be resized.
+	var a [10]int
+
+	a[0] = 'a'
+	fmt.Printf("%c\n", a[0])
+	fmt.Println(a) // [97 0 0 0 0 0 0 0 0 0]
+
+	// Array literals
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	primes2 := []int{2, 3, 5}
+	fmt.Println(primes2)
+}
